72a6767149b0602d44ed23ade102d7df83c8598e
suhailmoidin <suhailmoidin96@gmail.com>
2025-02-04 16:49:43 +0530

    Build error fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ import { BillingProvider } from '@/providers/billing-provider'
 const font = DM_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Fuzzie.',
-  description: 'Automate Your Work With Fuzzie.',
+  title: 'Otomato',
+  description: 'Automate Your Work With Otomato',
 }
 
 export default function RootLayout({
--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -26,7 +26,7 @@ const MenuOptions = (props: Props) => {
           className="flex font-bold flex-row "
           href="/"
         >
-          fuzzie.
+          Otomato.
         </Link>
         <TooltipProvider>
           {menuOptions.map((menuItem) => (
